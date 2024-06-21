421ea153ef5b1b403ddac8f7c5511b947741e817
Kaszio <senne.wittock@gmail.com>
2024-06-21 18:23:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ getapi(api_url);
 
 function show(data){
   let ram = document.getElementById("totalRam");
-  ram.innerHTML = data.TotalRAM / 1000 + " TB";
+  ram.innerHTML = Math.round(data.TotalRAM*100 / 1000 )/100+ " TB";
   let cores = document.getElementById("totalCores");
   cores.innerHTML = data.TotalCore;
   let benchmarks = document.getElementById("benchmarks");
@@ -157,7 +157,7 @@ function show(data){
   let cpu = document.getElementById("cpu");
 cpu.innerHTML = data.TotalGPU;
 let storage = document.getElementById("storage");
-storage.innerHTML = data.TotalStorage / 1000 + " TB";
+storage.innerHTML = Math.round(data.TotalStorage*100 / 1000 )/100+ " TB";
 }
 
 // api url
@@ -235,7 +235,7 @@ function show2(data) {
     <span class="span2">${r.Computer.cpus[0].num_cores} CORES</span>
     </td> 
     <td>${r.Computer.gpus[0].model}</td>
-    <td>${Math.floor(r.Computer.TotalMemory / 1024)+ " GB"}</td>
+    <td>${Math.round(r.Computer.TotalMemory * 100 / 1024 ) / 100+ " GB"}</td>
     <td>${r.Computer.storages[0].manufacturer}</td>
     <td>${r.Score}</td></tr>`
     let numr = `${r.Rank}`;
